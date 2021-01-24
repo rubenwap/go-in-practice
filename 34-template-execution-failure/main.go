5d137935037021ef6a64558a97538cf4045e1fdd
Ruben Sanchez <r@rubensanchez.dev>
2021-01-24 16:12:30 +0100

    Added up to technique 36

---
+++ 34-template-execution-failure/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var t *template.Template
+
+func init() {
+	t = template.Must(template.ParseFiles("./templates/simple.html"))
+}
+
+type Page struct {
+	Title, Content string
+}
+
+func displayPage(w http.ResponseWriter, r *http.Request) {
+	p := &Page{Title: "An Example", Content: "Have fun stormin da castle"}
+	var b bytes.Buffer
+	err := t.Execute(&b, p)
+	if err != nil {
+		fmt.Fprint(w, "An error occured")
+		return
+	}
+	b.WriteTo(w)
+}
+
+func main() {
+	http.HandleFunc("/", displayPage)
+	http.ListenAndServe(":8080", nil)
+}
